b164a0ce2371ff04a37b8561d72f63f4089305bd
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-04-14 21:49:50 -0500

    Page display and create implementation

--- controllers/guitarController.js
+++ controllers/guitarController.js
@@ -1,35 +1,62 @@
-const req = require('express/lib/request');
-const res = require('express/lib/response');
 var Guitar = require('../models/guitarSchema');
 
-//List of all guitars
-exports.guitar_list = async function(req, res){
+//List of all Guitars
+exports.guitar_list = async function(req, res) {
     try{
         theGuitars = await Guitar.find();
         res.send(theGuitars);
     }
     catch(err){
         res.status(500);
-        res.send(`{"error":${err}}`);
+        res.send(`{"error": ${err}}`);
     }
 };
 
-//For a specific guitar
+//For a specific Guitar
 exports.guitar_detail = function(req, res){
     res.send('NOT IMPLEMENTED: Guitar detail: ' + req.params.id);
 };
 
-//Handle guitar create on POST
-exports.guitar_create_post = function(req,res){
-    res.send('NOT IMPLEMENTED: Guitar create POST');
+//Handle Guitar create on POST
+exports.guitar_create_post = function(req, res){
+    console.log(req.body)
+    let document = new Guitar();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object.
+    document.guitar_type = req.body.guitar_type;
+    document.size = req.body.size;
+    document.price = req.body.price;
+    try{
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{"error": ${err}}`);
+    }
 };
 
-//Handle guitar delete form on DELETE
+//Handle Guitar delete form on DELETE
 exports.guitar_delete = function(req, res){
     res.send('NOT IMPLEMENTED: Guitar delete DELETE ' + req.params.id);
 };
 
-//Handle guitar update form on PUT
+//Handle Guitar update form on PUT
 exports.guitar_update_put = function(req, res){
     res.send('NOT IMPLEMENTED: Guitar update PUT' + req.params.id);
 };
+
+//VIEWS
+// Handle a show all view
+exports.guitar_view_all_Page = async function(req, res) {
+    try{
+        theGuitars = await Guitar.find();
+        res.render('guitars', { title: 'Guitar Search Results', results: theGuitars });
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+   };
+   
--- routes/guitar.js
+++ routes/guitar.js
@@ -1,9 +1,7 @@
 var express = require('express');
+const guitar_controllers = require('../controllers/guitarController');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('guitar', { title: 'Search Results' });
-});
-
+/* GET guitars */
+router.get('/', guitar_controllers.guitar_view_all_Page);
 module.exports = router;
