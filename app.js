991c8f844105fbc9f7f39d285c7f5fab4b3475df
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-04-22 13:01:56 -0500

    saved progress from last session

--- app.js
+++ app.js
@@ -104,7 +104,6 @@ app.use('/resource', resourceRouter);
 // Use the existing connection
 // The Account model
 var Account =require ('./models/account');
-
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
